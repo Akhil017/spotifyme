360f53430966f3f9241156012b995514b37d39b5
Akhil017 <akhil.17.akhi@gmail.com>
2024-07-13 23:57:46 +0530

    added followers and popularity chart

---
+++ src/app/dashboard/_components/charts/followers-bar-chart.tsx
@@ -0,0 +1,103 @@
+"use client";
+
+import {
+  Bar,
+  BarChart,
+  CartesianGrid,
+  LabelList,
+  XAxis,
+  YAxis,
+} from "recharts";
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  ChartConfig,
+  ChartContainer,
+  ChartTooltip,
+  ChartTooltipContent,
+} from "@/components/ui/chart";
+
+const chartConfig = {
+  desktop: {
+    label: "Desktop",
+    color: "hsl(var(--chart-1))",
+  },
+  mobile: {
+    label: "Mobile",
+    color: "hsl(var(--chart-2))",
+  },
+  label: {
+    color: "hsl(var(--background))",
+  },
+} satisfies ChartConfig;
+
+type FollowersBarChartProps = {
+  data: { artist: string; followers: number; image: string }[];
+};
+
+export function FollowersBarChart({ data }: FollowersBarChartProps) {
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>Artist - Followers</CardTitle>
+        <CardDescription>Followers bar chart</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <ChartContainer
+          config={chartConfig}
+          className="mx-auto aspect-auto h-[200px]"
+        >
+          <BarChart
+            accessibilityLayer
+            data={data}
+            layout="vertical"
+            margin={{
+              right: 0,
+              left: 25,
+            }}
+          >
+            <CartesianGrid horizontal={false} />
+            <YAxis
+              dataKey="artist"
+              type="category"
+              tickLine={false}
+              tickMargin={10}
+              axisLine={false}
+            />
+            <XAxis dataKey="followers" type="number" hide />
+            <ChartTooltip
+              cursor={false}
+              content={<ChartTooltipContent indicator="line" />}
+            />
+            <Bar
+              dataKey="followers"
+              layout="vertical"
+              fill="var(--color-desktop)"
+              radius={4}
+            >
+              <LabelList
+                dataKey="followers"
+                position="right"
+                offset={8}
+                className="fill-foreground"
+                fontSize={12}
+              />
+            </Bar>
+          </BarChart>
+        </ChartContainer>
+      </CardContent>
+      <CardFooter className="flex-col items-start gap-2 text-sm">
+        <div className="flex gap-2 font-medium leading-none">
+          Your top 5 artists and no of their followers
+        </div>
+      </CardFooter>
+    </Card>
+  );
+}
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,11 +2,26 @@ import { auth } from "@/auth";
 import { getTopArtist } from "./_actions/spotify-apis";
 import Image from "next/image";
 import FollowersRadialChart from "./_components/charts/followers-radial-chart";
-import PopularityPieChart from "./_components/charts/popularity-pie-chart";
+import { FollowersBarChart } from "./_components/charts/followers-bar-chart";
+import Link from "next/link";
 
 export default async function Page() {
   const session = await auth();
   const data = await getTopArtist(session?.accessToken!);
+
+  const followersData =
+    data?.items?.slice(0, 5)?.map((item) => ({
+      artist: item.name,
+      image: item.images[0].url,
+      followers: item.followers.total,
+    })) || [];
+
+  const populairtyData =
+    data?.items?.slice(0, 5)?.map((item, index) => ({
+      artist: item.name,
+      popularity: item.popularity,
+      fill: `hsl(var(--chart-${index + 1}))`,
+    })) || [];
 
   if (!data) return <p>No data</p>;
 
@@ -19,22 +34,25 @@ export default async function Page() {
             className="flex items-center flex-col gap-2 justify-center cursor-pointer"
           >
             <div className="col-span-1 w-24 h-24 md:w-32 md:h-32 relative rounded-full border-4 border-primary">
-              <Image
-                src={item.images[0].url}
-                className="rounded-full"
-                alt="Picture of the author"
-                fill
-                style={{ objectFit: "cover" }}
-              />
+              <Link href={item.href} target="_blank">
+                <Image
+                  src={item.images[0].url}
+                  className="rounded-full"
+                  alt="Picture of the author"
+                  fill
+                  style={{ objectFit: "cover" }}
+                />
+              </Link>
             </div>
             <p>{item.name}</p>
           </div>
         ))}
       </div>
       <div className="w-full grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
-        <FollowersRadialChart />
-        <PopularityPieChart />
-        <FollowersRadialChart />
+        <div className="col-span-2">
+          <FollowersBarChart data={followersData} />
+        </div>
+        <FollowersRadialChart data={populairtyData} />
       </div>
     </main>
   );
