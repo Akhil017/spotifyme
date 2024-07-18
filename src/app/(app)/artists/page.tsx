9d1067ec864a4d8c1b93895b6ed5aeb1abd058ba
Akhil017 <akhil.17.akhi@gmail.com>
2024-07-18 23:32:30 +0530

    added artist page

--- src/app/(app)/artists/page.tsx
+++ src/app/(app)/artists/page.tsx
@@ -1,3 +1,46 @@
-export default function Artists() {
-  return <div>Artists</div>;
+import { auth } from "@/auth";
+import { getTopArtist } from "../_actions/spotify-apis";
+import Image from "next/image";
+import { Badge } from "@/components/ui/badge";
+import Link from "next/link";
+
+export default async function Artists() {
+  const session = await auth();
+  const artistList = await getTopArtist(session?.accessToken!, 50);
+
+  if (!artistList) return <p>no data</p>;
+
+  return (
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 lg:gap-6">
+      {artistList?.items?.map((artist) => (
+        <Link key={artist.id} href={`/artists/${artist.id}`}>
+          <div className="p-4 border rounded-lg flex gap-2 hover:border-primary duration-300">
+            <div className="size-20  md:size-28 relative rounded-full border-[3px] border-primary duration-300 shrink-0">
+              <Image
+                src={artist.images[0].url}
+                className="rounded-full"
+                alt="Picture of the author"
+                fill
+                style={{ objectFit: "cover" }}
+              />
+            </div>
+            <div className="space-y-2">
+              <h4>{artist?.name}</h4>
+              <div className="flex gap-2 flex-wrap">
+                {artist?.genres?.map((genre) => (
+                  <Badge
+                    key={genre}
+                    variant="secondary"
+                    className="text-xs font-normal"
+                  >
+                    {genre}
+                  </Badge>
+                ))}
+              </div>
+            </div>
+          </div>
+        </Link>
+      ))}
+    </div>
+  );
 }
